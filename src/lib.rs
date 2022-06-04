30e8c1f67bf9b26dfdab06e73c44e5e22b9bbc03
Thomas Ip <thomas@ipthomas.com>
2022-06-04 13:31:29 +0800

    Add Pubkey serde deserializer

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,40 @@
+use std::str::FromStr;
+
+use serde::de::Error;
+use serde::{Deserialize, Deserializer};
+use solana_sdk::pubkey::Pubkey;
+
 pub mod serum;
+pub mod spl_token;
+
+pub fn deserialize_pubkey<'de, D>(deserializer: D) -> Result<Pubkey, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: &str = Deserialize::deserialize(deserializer)?;
+    Pubkey::from_str(&s).map_err(|_| D::Error::custom(format!("invalid base58 address: {}", s)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Deserialize)]
+    struct PubkeyContainer {
+        #[serde(deserialize_with = "deserialize_pubkey")]
+        pubkey: Pubkey,
+    }
+
+    #[test]
+    fn test_deserialize_pubkey() {
+        let input = r#"{ "pubkey": "So11111111111111111111111111111111111111112" }"#;
+        let PubkeyContainer { pubkey } = serde_json::from_str(input).unwrap();
+        assert_eq!(
+            pubkey,
+            Pubkey::from_str("So11111111111111111111111111111111111111112").unwrap()
+        );
+
+        let malformed_input = r#"{ "pubkey": "Solana1111111111111111111111111111111111112" }"#;
+        assert!(serde_json::from_str::<PubkeyContainer>(malformed_input).is_err());
+    }
+}
---
+++ src/spl_token.rs
@@ -0,0 +1,49 @@
+use serde::Deserialize;
+use serde_repr::Deserialize_repr;
+use solana_sdk::pubkey::Pubkey;
+use thiserror::Error;
+
+use crate::deserialize_pubkey;
+
+const TOKEN_LIST_URL: &str = "https://raw.githubusercontent.com/solana-labs/token-list/main/src/tokens/solana.tokenlist.json";
+
+#[derive(Debug, Deserialize)]
+pub struct TokenListWrapper {
+    tokens: Vec<TokenInfo>,
+}
+
+#[derive(Debug, Deserialize_repr)]
+#[repr(u8)]
+pub enum ClusterSlug {
+    MainnetBeta = 101,
+    Testnet = 102,
+    Devnet = 103,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TokenInfo {
+    #[serde(rename = "chainId")]
+    pub cluster_slug: ClusterSlug,
+    #[serde(deserialize_with = "deserialize_pubkey")]
+    pub address: Pubkey,
+    pub name: String,
+    pub decimals: u32,
+    pub symbol: String,
+}
+
+#[derive(Debug, Error)]
+pub enum TokenListError {
+    #[error("network error")]
+    Network(#[from] reqwest::Error),
+    #[error("malformed API response")]
+    Deserialize(#[from] serde_json::Error),
+}
+
+pub async fn fetch_token_list() -> Result<Vec<TokenInfo>, TokenListError> {
+    let wrapper = reqwest::get(TOKEN_LIST_URL)
+        .await?
+        .json::<TokenListWrapper>()
+        .await?;
+    Ok(wrapper.tokens)
+}
